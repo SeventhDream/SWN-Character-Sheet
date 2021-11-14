fd360b4148c759e199d85a760a02772bfef2c077
SeventhDream <68449189+SeventhDream@users.noreply.github.com>
2021-11-15 10:16:40 +1300

    Updated sidepanel menu

---
+++ header.php
@@ -0,0 +1,12 @@
+<input type="button" class="menuButton button" onclick="menuOpen()" value="☰">
+            <div id="myMenuBar" class="sideMenuPanel">
+                <a href="javascript:void(0)" class="closebtn" name="menuCloseBtn" onclick="menuClose()">×</a>
+                <br>
+                <br>
+                <a class="active" href="#home">Character</a>
+                <a href="#news">Signup</a>
+                <a href="#news">Login</a>
+                <a href="#news">Profile Page</a>
+                <a href="#contact">Contact</a>
+                <a href="#about">About</a>
+                </div>
